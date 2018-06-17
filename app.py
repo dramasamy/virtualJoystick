a6668f71d8109182e44352ee489ed73c676d83bd
Dinesh Kumar Ramasamy <dramasam@Dineshs-MacBook-Pro.local>
2018-06-16 20:44:08 -0500

    added host name port option

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 """
-Demo Flask application to test the operation of Flask with socket.io
-
-Aim is to create a webpage that is constantly updated with random numbers from a background python process.
-
-30th May 2014
-
-===================
-
-Updated 13th April 2018
-
-+ Upgraded code to Python 3
-+ Used Python3 SocketIO implementation
-+ Updated CDN Javascript and CSS sources
-
+Create a webpage that is constantly updated with random numbers from a background python process.
 """
-
-
-
 
 # Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
@@ -90,4 +74,4 @@ def test_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='127.0.0.1', port=5000)
